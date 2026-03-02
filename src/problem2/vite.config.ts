b1231ce14de6a2e7a24adcaedd844ab2bae1c67b
nphuoctho <nguyenphuoctho0310@gmail.com>
2026-03-02 13:58:33 +0700

    feat: setup theme color and setup font for problem 2

--- src/problem2/vite.config.ts
+++ src/problem2/vite.config.ts
@@ -1,6 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    ViteWebfontDownload(
+      "https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+    ),
+  ],
 });
