56e6d0dc6278158e512e9d5c55e96dba4ddd60f3
nphuoctho <nguyenphuoctho0310@gmail.com>
2026-03-10 17:20:49 +0700

    fix: download local font

--- src/problem2/vite.config.ts
+++ src/problem2/vite.config.ts
@@ -3,10 +3,5 @@ import { defineConfig } from "vite";
 import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    ViteWebfontDownload(
-      "https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
-    ),
-  ],
+  plugins: [tailwindcss(), ViteWebfontDownload()],
 });
